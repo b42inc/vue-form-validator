28648c780bab02282eb1e1e3bce9a1103cc32c92
NaokiMURATA <murata@b42.co.jp>
2019-12-28 07:40:42 +0900

    update @types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,5 +1,3 @@
-import { VNode } from "vue/types/umd"
-
 export type HTMLFieldElement = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
 
 export type FieldOptions = {
@@ -23,37 +21,4 @@ export type ValidateRuleset = string | {
     rule:string,
     error?:ValidateError,
     options?:any
-}
-
-export interface Validator {
-    check(code:string, el:HTMLFieldElement):(null | {msg:String, rule:string})[]
-    defineRule(validCode:string, validate:ValidateFunc, error:ValidateError, defaultOptions?:any):void
-    difineRuleset(setCode:string, ruleset:ValidateRuleset[]):void
-}
-
-export declare class FieldValue {
-    constructor(name:string, initValue:string, validations:FiledValueValidations)
-    name: string
-    value: string
-    valid: boolean
-    errors: {[key:string]: string}
-    _validations: FiledValueValidations
-    set validations(v:FiledValueValidations)
-    get validations():FiledValueValidations
-    resetError():void
-    hasError():boolean
-    eachValidation(el:HTMLFieldElement, eventName:string, callback:Function):void
-}
-
-export declare class Field {
-    constructor(el:HTMLFieldElement, vnode:VNode, value:FieldValue, options:FieldOptions)
-    get errors():{[key:string]:string}
-    get noValidate():boolean
-    get name():string
-    get value():string
-    set value(value:string) 
-    preventResolve(dynamicErrorMessage:string):void
-    preventReject():void
-    updateEvent(events:string[]):void
-    dispose():void
 }
